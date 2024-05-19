cbf036884b405fa45b6680934c945d8e465f9453
TettaTakahashi <x4tetta@gmail.com>
2024-05-19 01:46:41 +0000

    install Rubocop

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,51 +1,53 @@
+# frozen_string_literal: true
+
 class BooksController < ApplicationController
   def new
   end
 
   def index
-    @books=Book.all
-    @book=Book.new
+    @books = Book.all
+    @book = Book.new
   end
-  
+
   def create
-    @book=Book.new(book_params)
+    @book = Book.new(book_params)
     if @book.save
-      flash[:notice]="Book was successfully created."
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      @books=Book.all
-      render:index
+      @books = Book.all
+      render :index
     end
   end
 
   def show
-    @book=Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
-    @book=Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
-  
+
   def destroy
-    @book=Book.find(params[:id])
+    @book = Book.find(params[:id])
     @book.destroy
-    flash[:notice]="Book was successfully destroyed."
-    redirect_to'/books'
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to "/books"
   end
-  
+
   def update
-    @book=Book.find(params[:id])
+    @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice]="Book was successfully updated."
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
-      @books=Book.all
-      render:edit
+      @books = Book.all
+      render :edit
     end
   end
-  
+
   private
-  def book_params
-    params.require(:book).permit(:title,:body)
-  end
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 end
